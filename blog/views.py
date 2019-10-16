3121625edce013fc28dfa7b391e1ddb3c904b0e5
Allwin <allwindicaprio@gmail.com>
2019-10-16 19:45:35 +0530

    added api integration in template

--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,58 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.core.mail import send_mail
 import wikiquote
+import json
+import requests
 
 
 def wallethome(request):
-    return render(request,'blog/home.html');
+    return render(request, 'blog/home.html')
+
 
 def wallet(request):
-    return render(request,'blog/wallet.html');
+    return render(request, 'blog/wallet.html')
+
 
 def importwallet(request):
-    return render(request,'blog/import.html');
+    return render(request, 'blog/import.html')
+
 
 def post_list(request):
-    str=wikiquote.quote_of_the_day()
-    quote=str[0]
-    author=str[1]
-    context={'quote':quote,'author':author}
-    return render(request, 'blog/post_list.html',context)
+    str = wikiquote.quote_of_the_day()
+    quote = str[0]
+    author = str[1]
+    context = {'quote': quote, 'author': author}
+    return render(request, 'blog/post_list.html', context)
+
 
 def contact(request):
-    return render(request, 'blog/contact.html',{})
+    return render(request, 'blog/contact.html', {})
+
 
 def resume(request):
-    return render(request,'blog/resume.html',{})
+    return render(request, 'blog/resume.html', {})
+
 
 def home(request):
-    return render(request,'blog/post_list.html',{})
+    return render(request, 'blog/post_list.html', {})
+
 
 def feedback(request):
     if request.method == 'POST':
         receiving_mail = request.POST['sender']
         feedback = request.POST['comment']
-        send_mail('New feedback','sender:'+receiving_mail+'\nfeedback:'+feedback, 'mailtoallwinraju@gmail.com', ['allwindicaprio@gmail.com'], fail_silently=False)
-        send_mail('greetings', 'Hi, Thank you for subscribing!', 'mailtoallwinraju@gmail.com', [receiving_mail], fail_silently=False)
+        send_mail('New feedback', 'sender:' + receiving_mail + '\nfeedback:' + feedback, 'mailtoallwinraju@gmail.com',
+                  ['allwindicaprio@gmail.com'], fail_silently=False)
+        send_mail('greetings', 'Hi, Thank you for subscribing!', 'mailtoallwinraju@gmail.com', [receiving_mail],
+                  fail_silently=False)
         return HttpResponse('Subscribed')
     else:
-        return render(request,'blog/feedback.html')
+        return render(request, 'blog/feedback.html')
+
 
 def blog(request):
-    return render(request,'blog/blog.html')
+    response = requests.get("http://allwinblog.pythonanywhere.com/api/post/")
+    if response.status_code == 200:
+        print(response.json)
+        data = json.loads(response.text)[0]
+    return render(request, 'blog/blog.html', {'context': data})
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/allwin/Desktop/allwin12.pythonanywhere.com/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
